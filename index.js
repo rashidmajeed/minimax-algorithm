76964975fe12dbd5caec4c4c5ee97c961584cd5d
ChrisDillinger <dillingerdevelopment@gmail.com>
2019-08-04 13:10:25 -0400

    feat(index): Implement minimax function
    The minimax function determines all possible ways the game could go and returns the 'value' of the board

--- index.js
+++ index.js
@@ -60,17 +60,21 @@ function calculateWinner(squares) {
     let blankSpaceExists = false;
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
+      // if 3 matches, return winner letter immediately
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }
-      if(!squares[a] || !squares[b] || !squares[c]){
+      // track if there are any open spaces left
+      else if(!squares[a] || !squares[b] || !squares[c]){
         blankSpaceExists = true;
       }
     }
 
+    // if there are, return null, board is not in terminal state
     if(blankSpaceExists){
         return null;
     }
+    // otherwise board is full, it is a draw
     else {
         return 'draw';
     }
@@ -107,3 +111,39 @@ o -10
 x 10
 draw 0
 `);
+
+function isInTerminalState(board){
+  return calculateWinner(board) !== null;
+}
+
+function minimax(board, depth, isMaximizerPlayer){
+
+  const player = 'x';
+  const opponent = 'o';
+
+  if(isInTerminalState(board)){
+    return evaluate(calculateWinner(board));
+  }
+
+  if(isMaximizerPlayer){
+    let bestValue = -Infinity;
+    for(let i=0; i<board.length; i++){
+      if(!board[i]){
+        board[i] = player;
+        bestValue = Math.max(bestValue, minimax(board, depth+1, !isMaximizerPlayer));
+        board[i] = undefined;
+      }
+    }
+    return bestValue;
+  }
+  else{
+    let bestValue = Infinity;
+    for(let i=0; i<board.length; i++){
+      if(!board[i]){
+        board[i] = opponent;
+        bestValue = Math.min(bestValue, minimax(board, depth+1, !isMaximizerPlayer));
+        board[i] = undefined;
+      }
+    }
+  }
+}
